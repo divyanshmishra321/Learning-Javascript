3603d033d3da263c8f57eb09d9cdeaab7192b140
Divyansh Mishra <88049230+divyanshmishra321@users.noreply.github.com>
2023-12-09 12:46:05 +0530

    callback.js

---
+++ callback.js
@@ -0,0 +1,10 @@
+
+// filtering of array
+let number=[1,4,5,-7,-8,-9,0,0.5]
+let filtered= number.filter(value => value<=0);
+console.log(filtered);
+
+// Mapping of an array
+let numbers=[2,3,4,5,4,5,9,0];
+let items=numbers.map(value=>'Student_no '+ value);
+console.log(items);
